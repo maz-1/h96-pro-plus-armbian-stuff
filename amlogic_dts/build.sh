e56ca0580e9e509539b059d3d64abfd40921da4a
maz-1 <ohmygod19993@gmail.com>
2021-03-15 23:19:50 +0800

    update

--- amlogic_dts/build.sh
+++ amlogic_dts/build.sh
@@ -2,9 +2,7 @@
 LINUX_HEADER="$(pwd)/include"
 
 
-cpp -nostdinc -I $LINUX_HEADER -I arch -undef -x assembler-with-cpp meson-gxm-q200-mod.dts meson-gxm-q200-mod-preprocessed.dts || exit
+cpp -nostdinc -I $LINUX_HEADER -I arch -undef -x assembler-with-cpp meson-gxm-q200.dts meson-gxm-q200-mod-preprocessed.dts || exit
 dtc -I dts -O dtb -p 0x1000 meson-gxm-q200-mod-preprocessed.dts -o meson-gxm-q200-mod.dtb || exit
 rm meson-gxm-q200-mod-preprocessed.dts
-dtc -I dtb -O dts meson-gxm-q200-mod.dtb -o meson-gxm-q200-mod-decompiled.dts
-
-
+#dtc -I dtb -O dts meson-gxm-q200-mod.dtb -o meson-gxm-q200-mod-decompiled.dts
